a8d1c7d26df273a4bb9d3b384c68c32974c9530f
Diego Alexandro de Oliveira <diegoalexandrooliveira@gmail.com>
2020-08-17 21:40:03 -0300

    remove anotação

--- src/main/java/br/com/jornadadeveficiente/mercadolivre/usuario/dominio/CriptografarSenha.java
+++ src/main/java/br/com/jornadadeveficiente/mercadolivre/usuario/dominio/CriptografarSenha.java
@@ -1,12 +1,11 @@
 package br.com.jornadadeveficiente.mercadolivre.usuario.dominio;
 
-import lombok.SneakyThrows;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.AttributeConverter;
 
 public class CriptografarSenha implements AttributeConverter<String, String> {
-  @SneakyThrows
+
   @Override
   public String convertToDatabaseColumn(String senha) {
     return new BCryptPasswordEncoder().encode(senha);
